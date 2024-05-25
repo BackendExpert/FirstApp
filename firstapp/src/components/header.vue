296ddfce6df0078409de366599b84432395405c1
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-05-25 11:57:30 +0530

    Create header.vue
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

---
+++ firstapp/src/components/header.vue
@@ -0,0 +1,36 @@
+<!-- src/components/Header.vue -->
+<template>
+    <header class="header">
+      <nav>
+        <ul>
+          <li><a href="#home">Home</a></li>
+          <li><a href="#about">About</a></li>
+          <li><a href="#services">Services</a></li>
+          <li><a href="#contact">Contact</a></li>
+        </ul>
+      </nav>
+    </header>
+  </template>
+  
+  <script>
+  export default {
+    name: 'Header'
+  }
+  </script>
+  
+  <style scoped>
+  .header {
+    background-color: #333;
+    padding: 1rem;
+  }
+  .header nav ul {
+    list-style-type: none;
+    display: flex;
+    justify-content: space-around;
+  }
+  .header nav ul li a {
+    color: white;
+    text-decoration: none;
+  }
+  </style>
+  
